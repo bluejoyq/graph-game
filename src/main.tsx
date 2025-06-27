1f77706b1befb83f8fa2dc8c51cf5b7c1799d372
bluejoyq <bluejoy32@gmail.com>
2025-06-28 04:03:47 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,6 @@ import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
 import App from './App.tsx';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyAgoSF2XaRaY63OE2ikwYneeWcfsXxyNK8',
   authDomain: 'graph-game-34224.firebaseapp.com',
@@ -20,9 +15,8 @@ const firebaseConfig = {
   measurementId: 'G-S4N6MHQ56E',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const _analytics = getAnalytics(app);
+getAnalytics(app);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
